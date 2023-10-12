07ea1739a56f930cffd885eb8b86e3b5c20a84e3
DenDub <freeeze1@ya.ru>
2023-10-12 19:37:40 +0300

    ТЗ 11. add-database

--- src/main/java/ru/yandex/practicum/filmorate/controller/RatingController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/RatingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/rating")
+@RequestMapping("/mpa")
 public class RatingController {
     private final RatingService ratingService;
 
